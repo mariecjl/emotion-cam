ebadb8f9aaaedf4251e7136d93b600752bd6ce40
marie <168664981+mariecjl@users.noreply.github.com>
2025-12-25 16:00:28 -0500

    Update dataset.py

--- cnn/dataset.py
+++ cnn/dataset.py
@@ -5,19 +5,23 @@ from torch.utils.data import Dataset
 
 class FER2013Dataset(Dataset):
     def __init__(self, csv_file, split="Training"):
+        #loading csv file into pandas
         df = pd.read_csv(csv_file)
         df = df[df["Usage"] == split]
-
+        #extracting labels & image data
         self.labels = df["emotion"].values
         self.images = df["pixels"].values
 
     def __len__(self):
+        #total number of samples
         return len(self.labels)
 
     def __getitem__(self, idx):
+        #obtaining pixel string and reshaping into the image's coordinates
         img = np.array(self.images[idx].split(), dtype=np.float32)
-        img = img.reshape(1, 48, 48) / 255.0  # (1,48,48)
-
+        img = img.reshape(1, 48, 48) / 255.0  
+        #get the corresponding label
         label = self.labels[idx]
 
+        #return labeled image
         return torch.tensor(img), torch.tensor(label)
